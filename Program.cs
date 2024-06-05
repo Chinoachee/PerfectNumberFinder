809cfde1d177d8a9eea3d4288a1a4358e5c432f6
Chinoachee <djironov@mail.ru>
2024-06-06 00:33:55 +0400

    hp

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿Console.Write("Ищем идеальные числа до: ");
-int range = int.Parse(Console.ReadLine());
+long range = long.Parse(Console.ReadLine());
 int number = 0;
-int[] numbers = new int[range];
+List<int> numbers = new List<int>();
 int iterator = 0;
 for (int i = 1; i < range; i++) {
     for(int j = 1; j < i; j++) {
         if(i % j == 0) {
-            numbers[iterator++] = j;
+            numbers.Add(j);
         }
     }
-    for(int it = 0; it <= iterator; it++) {
-        number += numbers[it];
+    foreach(int j in numbers) {
+        number += j;
     }
     if(number == i) {
         Console.WriteLine($"Найдено совершенное число {i} ");
         Console.ReadKey();
     }
     Console.WriteLine(i);
-    iterator = 0;
     number = 0;
-    numbers = new int[range / 10];
+    numbers.Clear();
 }
